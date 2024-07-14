ffe4268d7ac1178dd8561857de8b0156271437c1
AwakeningSoul <weathergoals@gmail.com>
2024-07-14 07:46:27 +0100

    Add project files.

---
+++ ConsoleAppAssigment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleAppAssigment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a do while loop
+            int count = 0;
+            do
+            {
+                Console.WriteLine("Inside the loop");
+                count++; //add one to count each time
+            }
+            while (count < 5); //Continue to loop until count reaches 5
+
+            //This is a while loop
+            int counting = 0;
+            while (counting < 3) //countinue to loop until counting reaches 3
+            {
+                Console.WriteLine("Counting numbers");
+                counting++; //add one to counting each time
+            }
+            Console.ReadLine();
+        }
+    }
+}
